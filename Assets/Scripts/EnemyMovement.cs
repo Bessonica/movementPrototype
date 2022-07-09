f29c1fb14fe3360ad5cc3a644a414682c8cca146
Bessonica <studies.vuz@gmail.com>
2022-07-09 22:40:42 +0300

    finished turnwaypoint, everything works

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -49,7 +49,10 @@ public class EnemyMovement : MonoBehaviour
         // WayArray += WayThree;
         // WayArray += WayFour;
         // UnityEngine.Debug.Log(WayArray);
+
+        // engine creates CLONE of wayOne, does it take resources?
         currentWay = Instantiate(WayOne);
+
         // UnityEngine.Debug.Log(currentWay.name);
         wavePointIntIndex = 0;
         target = currentWay.points[0];
@@ -77,20 +80,14 @@ public class EnemyMovement : MonoBehaviour
     
     void Update()
     {
-        
-// transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
 
-// target = currentWay.points[wavePointIntIndex];
 
-//          b 1
-//     a 0        c 2
-//  c - target  wavePointIntIndex + 2
+        UnityEngine.Debug.Log("wavePoint index " + wavePointIntIndex);
 
 
 
 
-// dir1 a - b
-// dir2 b - c
+
 
     
  // my vector solution
@@ -102,57 +99,57 @@ public class EnemyMovement : MonoBehaviour
         // transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
        
 
-
-                            // in future if optmization gonna suck, test each solution, which faster?
- // my lerp  solution (works shitty)
-
-        // float newTime = (startTime - Time.time)/speed;
-
-        // transform.position = testLerp(currentWay.points[0].position, currentWay.points[1].position, newTime);
-
 // unity lerp solution ( CHECK UNITY LERP DOC )  works, BUT only with manualy choosen points(with wavePointIntIndex it just breaks)    
 
-        float distCovered = (Time.time - startTime)*speed;
 
-        pointLength = Vector3.Distance(currentWay.points[wavePointIntIndex].position, currentWay.points[wavePointIntIndex+1].position);
-        // UnityEngine.Debug.Log(Vector3.Distance(transform.position, currentWay.points[wavePointIntIndex+1].position ) );
-        float fraction = distCovered/pointLength;
-        // UnityEngine.Debug.Log("fraction " + fraction);
-        UnityEngine.Debug.Log("index " + wavePointIntIndex);
-        
-    // from a to b   WORKS but with manually choosen points
-        // UnityEngine.Debug.Log("lerp for transform.position " + Vector3.Lerp(currentWay.points[wavePointIntIndex].position, currentWay.points[wavePointIntIndex+1].position, fraction));
-        // transform.position = Vector3.Lerp(currentWay.points[wavePointIntIndex].position, currentWay.points[wavePointIntIndex+1].position, fraction);
-        
-    // from a to c,  b as ?anker?   WORKS but with manually choosen points
-        // Vector3 dir1 = Vector3.Lerp(currentWay.points[0].position, currentWay.points[1].position, fraction);
-        // Vector3 dir2 = Vector3.Lerp(currentWay.points[1].position, currentWay.points[2].position, fraction);
-        // transform.position = Vector3.Lerp(dir1, dir2, fraction);
 
-        // Vector3 dir3 = Vector3.Lerp(dir1, dir2, fraction);
+        // float distCovered = (Time.time - startTime)*speed;
+
+        // pointLength = Vector3.Distance(currentWay.points[wavePointIntIndex].position, currentWay.points[wavePointIntIndex+1].position);
+        // // UnityEngine.Debug.Log(Vector3.Distance(transform.position, currentWay.points[wavePointIntIndex+1].position ) );
+        // float fraction = distCovered/pointLength;
+        // // UnityEngine.Debug.Log("fraction " + fraction);
+        // UnityEngine.Debug.Log("index " + wavePointIntIndex);
+
+
+
         
 
 
-//  !! POSIBLE SOLUTION !!    use lerp only to make points,  BUT use transform.Translate to move it
-        Vector3 dir = Vector3.Lerp(currentWay.points[wavePointIntIndex].position, currentWay.points[wavePointIntIndex+1].position, fraction);
-        // UnityEngine.Debug.Log("Direction " + dir);
-        // transform.Translate(dir.normalized * speed * Time.deltaTime,  Space.World);
-        //
-        // Alternative to  transform.Translate   (it fucking works, i hate myself and unity)
         var step =  speed * Time.deltaTime; 
-        transform.position = Vector3.MoveTowards(transform.position, dir, step);
+
+        if(currentWay.points[wavePointIntIndex].childCount > 0)
+        {
+            
+            //   .GetChild(0);
 
 
+            UnityEngine.Debug.Log("<color=blue> Object has children </color>" + currentWay.points[wavePointIntIndex].GetChild(0).position  );
+            Vector3 turndir1 = currentWay.points[wavePointIntIndex].GetChild(0).position;
+            Vector3 turndir2 = currentWay.points[wavePointIntIndex+1].position;
 
+            float distCovered = (Time.time - startTime)*speed;
+            pointLength = Vector3.Distance(currentWay.points[wavePointIntIndex].position, turndir2);
+            float fraction = distCovered/pointLength;
 
-        // Vector3 one = Vector3.Lerp(currentWay.points[0].position, currentWay.points[1].position, speed * Time.deltaTime);
-        // Vector3 two = Vector3.Lerp(currentWay.points[1].position, currentWay.points[2].position, speed * Time.deltaTime);
+            Vector3 dir1 = Vector3.Lerp(currentWay.points[wavePointIntIndex].position, turndir1, fraction);
+            Vector3 dir2 = Vector3.Lerp(turndir1, turndir2, fraction);
+            Vector3 dir3 = Vector3.Lerp(dir1, dir2, fraction);
 
-        // transform.position = Vector3.Lerp(one, two, speed * Time.deltaTime);
+            
+            transform.position = Vector3.MoveTowards(transform.position, dir3, step);
+        }
+        else
+        {
+            float distCovered = (Time.time - startTime)*speed;
+            pointLength = Vector3.Distance(currentWay.points[wavePointIntIndex].position, currentWay.points[wavePointIntIndex+1].position);
+            float fraction = distCovered/pointLength;
 
-//   t = speed * Time.deltaTime
-        // transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
-        // UnityEngine.Debug.Log(target.position);
+            Vector3 dir = Vector3.Lerp(currentWay.points[wavePointIntIndex].position, currentWay.points[wavePointIntIndex+1].position, fraction);
+            transform.position = Vector3.MoveTowards(transform.position, dir, step);
+        }
+        
+
 
 
 
@@ -172,11 +169,6 @@ public class EnemyMovement : MonoBehaviour
     }
 
 
-    public static Vector3 testLerp(Vector3 a, Vector3 b, float t)
-    {
-        return a+(a-b)*t;
-
-    }
 
     void GetNextWaypoint()
     {
@@ -201,15 +193,21 @@ public class EnemyMovement : MonoBehaviour
         z = Random.Range(-0.3f, 0.3f);
         randomVec = new Vector3(x, y, z);
 
+        wavePointIntIndex++;
+
+
+        //     !!!! important  !!!   it was   >= currentWay.points.Length -2
+
+        //   really fucking important
+        // last waypoint should be in same place as first waypoint of NEW way
         
-        //     !!!! important  !!!
-        if(wavePointIntIndex >= currentWay.points.Length -2 )
+        if(wavePointIntIndex >= currentWay.points.Length -1 )
         {
             UnityEngine.Debug.Log("   <color=yellow> __________ Get Next Way ________ </color>   ");
             GetNextWay();
         }
 
-        wavePointIntIndex++;
+        
 
         startTime = Time.time;
 
@@ -234,6 +232,7 @@ public class EnemyMovement : MonoBehaviour
                  UnityEngine.Debug.Log("   <color=green> __________ Get Next Way: changed way ________ </color>   ");
                  currentWay = Instantiate(WayTwo);
                  wavePointIntIndex = 0;
+                 UnityEngine.Debug.Log("   <color=green> __________ Get Next Way: wavePointIntIndex ________ </color> " + wavePointIntIndex);
 
                  
 
